0f6e6c2720f2a44821f8cd3393e45e29f17938b9
DevMinGeonPark <devmin7985@gmail.com>
2021-10-10 13:32:58 +0900

    paginate

--- controllers/posts/index.js
+++ controllers/posts/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const ctrl = require("./posts.ctrl");
+const paginate = require('express-paginate');
 
-router.get('/posts', ctrl.get_posts)
+
+router.get("/posts", paginate.middleware(5, 50) ,ctrl.get_posts);
+
+// router.post("/posts/:id", ctrl.get_posts_id);
 
 module.exports = router;
--- controllers/posts/posts.ctrl.js
+++ controllers/posts/posts.ctrl.js
@@ -1,9 +1,42 @@
 const models = require("../../models");
 
-exports.get_posts = async(_,res) => {
-    try{
-        res.json({status:"post main"});
-    } catch(e){
-        console.log(e);
+exports.get_posts = async(req, res) => {
+
+    try {
+  
+      const [posts] = await Promise.all([
+  
+        models.Posts.findAll({
+          limit: req.query.limit,
+          offset: req.offset,
+          order: [['createdAt', 'desc']]
+        }),
+  
+        models.Posts.count()
+      ]);
+  
+  
+      res.json({
+        posts
+      });
+  
+    } catch (e) {
+      console.log(e);
     }
+}
+
+exports.get_posts_id = async(req,res) => {
+    try {
+        const posts = await models.Posts.findOne({
+          where: {
+            id: req.params.id
+          },
+          include: ["Posts"]
+        });
+
+        res.json({posts});
+    
+      } catch (e) {
+        console.log(e);
+      }
 }
